4cd428f643107b8be9b2612cbaf7d5a79f13a016
Graham Shapiro <virtualgraham@gmail.com>
2020-10-27 16:59:21 +0800

    .

--- src/sc_patch_a.py
+++ src/sc_patch_a.py
@@ -418,10 +418,11 @@ global_val_loss = []
 
 last_epoch = 0
 
+
 training_image_paths = glob(f'model_{train_batch_size}_{num_epochs}_{learn_rate}_{patch_dim}_{gap}_*.pt')
 
 if len(training_image_paths) > 0:
-  
+  training_image_paths.sort()  
   model_save_path = training_image_paths[-1]
   try:
     print('Loading Checkpoint...', model_save_path)
