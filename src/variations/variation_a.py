d4e6d28a27b2a6928807b9eaa0d4ef0fcde783d1
Graham Shapiro <virtualgraham@gmail.com>
2020-11-10 10:17:44 +0800

    .

--- src/variations/variation_a.py
+++ src/variations/variation_a.py
@@ -45,10 +45,10 @@ print(device)
 training_image_paths = glob('/data/open-images-dataset/train/*.jpg')
 validation_image_paths = glob('/data/open-images-dataset/validation/*.jpg')
 
-train_dataset_length = 409600
-validation_dataset_length = 20480
-train_batch_size = 2048
-validation_batch_size = 2048
+train_dataset_length = 408576
+validation_dataset_length = 19968
+train_batch_size = 1536
+validation_batch_size = 1536
 num_epochs = 1500
 save_after_epochs = 1 
 backup_after_epochs = 5 
